d0591ce3eb16594007dc2ea88728d134c81b9217
Stan Lo <stan.lo@shopify.com>
2022-11-29 11:03:07 +0000

    2. Use TracePoint to detect line execution
    This commit demonstrates how to use TracePoint to achieve line detection.
    
    - `$ bin/debug app.rb` should set an noop breakpoint at `app.rb:1`
    - `$ bin/debug app.rb lib.rb 2` should set an noop breakpoint at `lib.rb:2`
    
    `ruby/debug` actually uses a more complicated but performant approach to
    implement line breakpoint. It finds the targeted code's instruction sequence
    and uses it as TracePoint's target.

--- lib/debugger.rb
+++ lib/debugger.rb
@@ -1,1 +1,27 @@
 puts "Debugger is loaded"
+
+class LineBreakpoint
+  def initialize(file, line)
+    @file = file
+    @line = line
+    @tp =
+      TracePoint.new(:line) do |tp|
+        # we need to expand paths to make sure they'll match
+        if File.expand_path(tp.path) == File.expand_path(@file) &&
+             tp.lineno == @line
+          puts "#{name} is triggered"
+        end
+      end
+  end
+
+  def name
+    "Breakpoint at #{@file}:#{@line}"
+  end
+
+  def enable
+    @tp.enable
+    puts "Breakpoint at #{@file}:#{@line} is activated"
+  end
+end
+
+LineBreakpoint.new(ENV["DEBUGGEE_FILE"], ENV["DEBUGGEE_LINE"].to_i).enable
