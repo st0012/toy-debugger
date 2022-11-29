d1af56db2010f0057365ebbc0dfcab0a936076dc
Stan Lo <stan.lo@shopify.com>
2022-11-29 11:16:19 +0000

    Implement breakpoint suspension and simple commands
    Commands:
    
    - `c[ontinue]` - Leave the current breakpoint and continue the program
    - `exit` - Exit the program right away
    
    Demo:
    
    ```
    $ bin/debug app.rb lib.rb 2
    Debugger is loaded
    Breakpoint at lib.rb:2 is activated
    Breakpoint at lib.rb:2 is triggered
    (debug) self
    => Foo
    (debug) exit
    ```

--- lib/debugger.rb
+++ lib/debugger.rb
@@ -1,27 +1,56 @@
+require "reline"
 puts "Debugger is loaded"
 
-class LineBreakpoint
-  def initialize(file, line)
-    @file = file
-    @line = line
-    @tp =
-      TracePoint.new(:line) do |tp|
-        # we need to expand paths to make sure they'll match
-        if File.expand_path(tp.path) == File.expand_path(@file) &&
-             tp.lineno == @line
-          puts "#{name} is triggered"
+module Debugger
+  class Session
+    def suspend(binding)
+      while input = Reline.readline("(debug) ")
+        case input
+        when "c", "continue"
+          break
+        when "exit"
+          exit
+        else
+          puts "=> " + eval_input(binding, input).inspect
         end
       end
+    end
+
+    def eval_input(binding, input)
+      binding.eval(input)
+    rescue Exception => e
+      puts "Evaluation error: #{e.inspect}"
+    end
   end
 
-  def name
-    "Breakpoint at #{@file}:#{@line}"
-  end
+  SESSION = Session.new
+  class LineBreakpoint
+    def initialize(file, line)
+      @file = file
+      @line = line
+      @tp =
+        TracePoint.new(:line) do |tp|
+          # we need to expand paths to make sure they'll match
+          if File.expand_path(tp.path) == File.expand_path(@file) &&
+               tp.lineno == @line
+            puts "#{name} is triggered"
+            SESSION.suspend(tp.binding)
+          end
+        end
+    end
 
-  def enable
-    @tp.enable
-    puts "Breakpoint at #{@file}:#{@line} is activated"
+    def name
+      "Breakpoint at #{@file}:#{@line}"
+    end
+
+    def enable
+      puts "#{name} is activated"
+      @tp.enable
+    end
   end
 end
 
-LineBreakpoint.new(ENV["DEBUGGEE_FILE"], ENV["DEBUGGEE_LINE"].to_i).enable
+Debugger::LineBreakpoint.new(
+  ENV["DEBUGGEE_FILE"],
+  ENV["DEBUGGEE_LINE"].to_i
+).enable
